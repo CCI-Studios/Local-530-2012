60a9c5d797314335e8be8b695f46e805ccf79697
Matt Elliott <melliott@ccistudios.com>
2012-10-11 11:37:05 -0400

    deploy and staging scripts setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 require "capistrano/ext/multistage"
 
-set :application, "APPLICATION_NAME"
+set :application, "lu530.com"
 
 # repository info
-set :repository,  "git@github.com:CCI-Studios/PROJECT.git"
+set :repository,  "git@github.com:CCI-Studios/Local-530-2012.git"
 set :scm, :git
 
 # ssh settings
-set :user, "USERNAME"
+set :user, "lu530"
 set :use_sudo, false
 
 # Joomla
