def825e1ce6018941d19ea9452c6c357f6b7d993
Matt Elliott <melliott@ccistudios.com>
2012-10-17 09:29:46 -0400

    added clear.js to scripts.js

--- template/templates/electricians/js/scripts.js
+++ template/templates/electricians/js/scripts.js
@@ -4,6 +4,7 @@
  * @codekit-prepend "lettering.js"
  * @codekit-prepend "html5.js"
  * @codekit-prepend "rollover.js"
+ * @codekit-prepend "clear.js"
  */
 
 window.addEvent('domready', function() {
